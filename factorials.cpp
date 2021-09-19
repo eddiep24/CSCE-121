141c13bc03cda6ccabdd7847959323fac861feef
Eddie Phillips <85954305+eddiep24@users.noreply.github.com>
2021-09-18 23:57:04 -0500

    Update factorials.cpp

--- factorials.cpp
+++ factorials.cpp
@@ -1,36 +1,3 @@
-// To run it is Ctrl + Option + N
-/*
-
-#include <iostream>
-
-// using namespace std;
-using std::cout; // Replaces std:: prefix for cout only
-
- int main() 
-{
-    cout << "Hello World";
-} 
-
-*/
-
-// A namespace is a method provided in C++ that allows us to 
-// group or structure related entities inside one category.
-
-// #include <header_file> or "header_file"
-
-/*
-#include <stdio.h>
-
-int main( void )
-{
-    printf( "Howdy World!" );
-
-    return 0;
-}
-
-*/
-
-
 #include <iostream>
 using namespace std;
 
@@ -51,34 +18,3 @@ int main()
     cin >> y;
     cout << factorials(y) << endl;
 }  
-
-/*
-int main()
-{
-int x; 
-cout << "Type a number: "; // Type a number and press enter
-cin >> x; // Get user input from the keyboard
-cout << "Your number is: " << x; // Display the input value
-}
-*/
-
-/*
-ESCAPE SEQUENCES
-\n is return and new line
-\t is tab (8 spaces)
-\" is double quote
-\' is single quote
-\\ is backslash
-\0 is null character
-*/
-
-// <data type> indentifier1,indentifier2,indentifier3
-// int age, iq, shoe_size;
-
-// <data type> indentifier = <literal>
-// <data type> indentifier = <constant>
-// int sum = 0;
-// int Dad_Age = Retirement_Age;
-// int My_Age = 18;
-// int num_dependents, staff salary = base_salary;
-// 
